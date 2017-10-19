160c966161821d2c96a274df6c5060b3e17e428e
ggramazio <giusva90.subscriptions@gmail.com>
2017-10-19 20:25:24 +0200

    NPM Scripts Part 1

---
+++ Week4/Bootstrap4/conFusion/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$("#mycarousel").carousel( { interval: 2000 } );
+	$("#carousel-button").click(function(){
+		if ($("#carousel-button").children("span").hasClass('fa-pause')) {
+			$("#mycarousel").carousel('pause');
+			$("#carousel-button").children("span").removeClass('fa-pause');
+			$("#carousel-button").children("span").addClass('fa-play');
+		}
+		else if ($("#carousel-button").children("span").hasClass('fa-play')){
+			$("#mycarousel").carousel('cycle');
+			$("#carousel-button").children("span").removeClass('fa-play');
+			$("#carousel-button").children("span").addClass('fa-pause');                    
+		}
+	});
+});
