20476cc9f087224184e285ece3089c8263dd93c4
ggramazio <giusva90.subscriptions@gmail.com>
2017-10-22 00:19:34 +0200

    Assignment 4: Bootstrap, JQuery and Sass

--- Week4/Bootstrap4/conFusion/js/scripts.js
+++ Week4/Bootstrap4/conFusion/js/scripts.js
@@ -12,4 +12,10 @@ $(document).ready(function(){
 			$("#carousel-button").children("span").addClass('fa-pause');                    
 		}
 	});
+	$('#reserveButton').click(function(){
+		$('#reserveModal').modal('show');
+	});
+	$('#loginButton').click(function(){
+		$('#loginModal').modal('show');
+	});
 });
